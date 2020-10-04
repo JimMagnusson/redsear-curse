b6db296635f659ad200a25a56383bc09fe73e473
Jim Magnusson <jim.magnusson97@gmail.com>
2020-10-04 21:24:08 +0200

    Added dialogue system

---
+++ Assets/DialogueManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Queue<string> sentences;
+    [SerializeField] Text nameText;
+    [SerializeField] Text dialogueText;
+    [SerializeField] GameObject dialogueBox;
+    [SerializeField] GameObject witch;
+    private string npcName;
+
+    private void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        dialogueBox.SetActive(true);
+        npcName = dialogue.name;
+        Debug.Log("Starting dialogue with " + npcName);
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+    void EndDialogue()
+    {
+        dialogueBox.SetActive(false);
+        Debug.Log("End of conversation");
+        WitchMovement witchMovement = witch.GetComponent<WitchMovement>();
+        if (npcName == "The Witch")
+        {
+            if(!witchMovement.IsMoving() && !witchMovement.IsAtTarget())
+            {
+                witchMovement.SetIsMoving(true);
+            }
+            witch.GetComponent<NPC>().dialogue.sentences = new string[] { "Oh my, you survived this long!", "There, now your curse is removed!" };
+            if (witchMovement.IsAtTarget())
+            {
+                FindObjectOfType<LoadHandler>().LoadNextScene();
+            }
+        }
+    }
+}
---
+++ Assets/WitchMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WitchMovement : MonoBehaviour
+{
+    [SerializeField] GameObject targetGO;
+    [SerializeField] float speed = 10f;
+    private Vector2 target;
+    private Vector2 position;
+    private bool isMoving = false;
+    private Animator animator;
+    private bool isAtTarget = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = targetGO.transform.position;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isMoving)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, target, step);
+            animator.SetBool("WitchWalking", true);
+        }
+        else
+        {
+            animator.SetBool("WitchWalking", false);
+        }
+        Vector2 myPos = new Vector2(transform.position.x, transform.position.y);
+        if(target == myPos)
+        {
+            if(!isAtTarget)
+            {
+                isAtTarget = true;
+            }
+        }
+    }
+
+    public void SetIsMoving(bool isMoving)
+    {
+        this.isMoving = isMoving;
+    }
+    public bool IsMoving()
+    {
+        return isMoving;
+    }
+
+    public bool IsAtTarget()
+    {
+        return isAtTarget;
+    }
+}
