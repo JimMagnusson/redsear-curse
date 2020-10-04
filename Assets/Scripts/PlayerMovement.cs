eff180efcab3b9ecef876195589319e0562441f0
Jim Magnusson <jim.magnusson97@gmail.com>
2020-10-04 08:21:04 +0200

    Added player run animation and switches when moving

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rigidBody;
+    bool facingLeft = true;
+    Vector3 lastPosition;
+    Animator animator;
 
+    [SerializeField] GameObject body;
     [SerializeField] float walkingSpeed = 5f;
+
 
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        lastPosition = transform.position;
+        animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         HandleInput();
+        CheckPositionChange();
+        lastPosition = transform.position;
     }
 
     void HandleInput()
@@ -27,5 +36,41 @@ public class PlayerMovement : MonoBehaviour
         movementVector.Normalize();
         Vector2 velocity = movementVector * walkingSpeed;
         rigidBody.velocity = velocity;
+    }
+
+    void CheckPositionChange()
+    {
+        float changeInXPos = lastPosition.x - transform.position.x;
+        float changeInYPos = lastPosition.y - transform.position.y;
+
+        if (Mathf.Abs(changeInXPos) >= Mathf.Epsilon) 
+        {
+            animator.SetBool("PlayerRunning", true);
+            if(changeInXPos < 0 && facingLeft) // Moving right
+            {
+                FlipSprite();
+                facingLeft = false;
+            }
+            else if(changeInXPos > 0 && !facingLeft)
+            {
+                FlipSprite();
+                facingLeft = true;
+            }
+        }
+
+        if(Mathf.Abs(changeInXPos) >= Mathf.Epsilon || Mathf.Abs(changeInYPos) >= Mathf.Epsilon)
+        {
+            animator.SetBool("PlayerRunning", true);
+        }
+        else
+        {
+            animator.SetBool("PlayerRunning", false);
+        }
+    }
+
+    void FlipSprite()
+    {
+        Vector3 bodyScale = body.transform.localScale;
+        body.transform.localScale = new Vector3(-bodyScale.x, bodyScale.y, bodyScale.z);
     }
 }
